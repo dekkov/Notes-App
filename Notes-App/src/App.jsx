b4c3fe6cefa84c23a498b346b776ac183f8b73b9
Hoang Tran <trhoang2003@gmail.com>
2024-03-30 10:49:10 -0700

    FInallize
    FInished

--- Notes-App/src/App.jsx
+++ Notes-App/src/App.jsx
@@ -48,7 +48,10 @@ export default function App() {
 
     React.useEffect(() => {
         const timeoutId = setTimeout(()=>{
-            updateNote(tempNoteText)
+            if ( tempNoteText !== currentNote.body) {
+
+                updateNote(tempNoteText)
+            }
         }, 500)
 
         return () => clearTimeout(timeoutId)
